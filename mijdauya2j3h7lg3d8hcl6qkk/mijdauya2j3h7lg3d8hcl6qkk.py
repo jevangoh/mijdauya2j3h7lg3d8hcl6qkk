c1d24d6e22a7bcdce175718e92e3cb84ae394013
Jevan Goh <javerngoh@gmail.com>
2026-09-23 22:05:51 +0800

    v0.0.1

--- mijdauya2j3h7lg3d8hcl6qkk/mijdauya2j3h7lg3d8hcl6qkk.py
+++ mijdauya2j3h7lg3d8hcl6qkk/mijdauya2j3h7lg3d8hcl6qkk.py
@@ -1,2 +1,10 @@
-def mijdauya2j3h7lg3d8hcl6qkk() -> None:
-    pass
+from typing import Any
+from structlog.typing import EventDict as EventDictionary
+
+
+def mijdauya2j3h7lg3d8hcl6qkk(
+    logger: Any, log_type_name: str, event_dict: EventDictionary
+) -> EventDictionary:
+    event_dict["level"] = log_type_name
+
+    return event_dict
